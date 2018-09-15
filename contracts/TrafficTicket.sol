aa8106199039c1e89461dc5a728e9a130ac628a9
dlastwishes <dlasterwishes@gmail.com>
2018-09-15 19:59:12 +0700

    edit dashboard and login html

--- contracts/TrafficTicket.sol
+++ contracts/TrafficTicket.sol
@@ -1,233 +1,152 @@
-pragma solidity ^0.4.17;
+pragma solidity^0.4.17;
 
 import "github.com/Arachnid/solidity-stringutils/src/strings.sol";
 
-contract trafficTicket {
-    using strings for *;
-    string[] trafficTicketNo;
+contract officer{
+     using strings for *;
+    address private addrSAdmin;
+    mapping (string => string ) private userandpass;
+    mapping ( address  => string)  prevLogin;
+    mapping (string => bool) permissionUser;
     
-    address owner = msg.sender;
-	
-    struct reporter {
-        string  rep_name;
-        string rep_unit;
-        string rep_address;
-        string rep_tel;
-        string rep_zipcode;
-    }
-	
-    struct report_case {
-        string  _charge;
-        string  _placeOfIncident;
-        string _speedDetection;
-        uint  _amountOfFine;
+    struct officerInfo {
+        string username;
+        string name;
+        string unit;
+        string office_address;
+        string tel;
+        string zipcode;
     }
     
-	   struct conveyanceOwner{
-			string conv_personalNo;
-			string conv_plateNo;
-			string conv_name;
-			string conv_tele;
-			string conv_address;
-		}
-	
-    struct TrafficTicket{
-        conveyanceOwner conveyList;
-        reporter reporterList;
-        report_case reportList;
-        
+    mapping (string => string ) mapOfficerToUnit;
+    
+    struct unitInfo {
+        string unitName;
+        string unitAddr;
+        string unitTel; 
+        string unitZipcode;
     }
-     
-    mapping(string => TrafficTicket) trafficList;
-	
-    conveyanceOwner private convey;
-    reporter private reportInfo;
-    report_case private report;
     
-    mapping (uint => conveyanceOwner) mapConvey;
-    mapping (uint => reporter) mapReporter;
-    mapping (uint => report_case) mapReportCase;
+    string[] unitList;
+    mapping (string => unitInfo) unitNoToInfo;
     
-    modifier onlyOwner() {
-        require(msg.sender == owner);
+    string[] private officerUsernameList;
+    
+    officerInfo private officerList;
+    mapping(string => mapping(string => officerInfo)) officerUser;
+    
+    
+        modifier onlyOwner(){
+        require(msg.sender == addrSAdmin);
         _;
     }
-	
-	modifier checkShowTicket(string _trafficticket , string personid ) {
-
-		require(_trafficticket.toSlice().len() > 0);
-		require(personid.toSlice().len() >0);
-		require(personid.toSlice().equals(trafficList[_trafficticket].conveyList.conv_personalNo.toSlice()));
-
-	_;
-	}
-	
-	function getTotalTicket() public view returns (uint) {
-	    return (trafficTicketNo.length);
-	}
-	
-	function getTicket(string ticketNo) public view returns (string){
-	    
-	    return (trafficList[ticketNo].conveyList.conv_name);
-	}
-	
     
-    function getConveyance(string id , string _personalid) public checkShowTicket(id , _personalid) constant returns (string , string , string , string , string)  {
+    function addUnit(string unitNo , string unitName , string unitAddr , string unitTel , string unitZipcode) public onlyOwner returns (bool) {
+        unitList.push(unitNo);
+        unitNoToInfo[unitNo].unitName = unitName;
+        unitNoToInfo[unitNo].unitAddr = unitAddr;
+        unitNoToInfo[unitNo].unitTel = unitTel;
+        unitNoToInfo[unitNo].unitZipcode = unitZipcode;
         
-        return (trafficList[id].conveyList.conv_personalNo
-        , trafficList[id].conveyList.conv_plateNo
-        , trafficList[id].conveyList.conv_name
-        , trafficList[id].conveyList.conv_tele 
-        , trafficList[id].conveyList.conv_address);
+        return (true);
     }
     
-    function getReporter(string id , string _personalid) public checkShowTicket(id , _personalid) constant returns (string , string , string , string , string){
-        return (trafficList[id].reporterList.rep_name
-         , trafficList[id].reporterList.rep_unit
-         
-         , trafficList[id].reporterList.rep_address , trafficList[id].reporterList.rep_tel
-         , trafficList[id].reporterList.rep_zipcode);
+    modifier checkOffice(string _usr) {
+        string memory pass = userandpass[_usr];
+        require(pass.toSlice().len() > 0);
+        _;
     }
     
-    function getReportCase(string id , string _personalid) public checkShowTicket(id , _personalid) constant returns ( string ,string ,string , uint){
-        
-        return (trafficList[id].reportList._charge , 
-        trafficList[id].reportList._placeOfIncident
-        , trafficList[id].reportList._speedDetection
-        , trafficList[id].reportList._amountOfFine);
-    
-    }
-
-  
-    
-    function newTrafficTicket( uint targetPoint ,string trafficID) public{
-	
-
-        report_case memory  reportSingletron;
-         reporter memory reporterSingletron;
-         conveyanceOwner memory conveySingletron;
-         
-         reportSingletron._charge = mapReportCase[targetPoint]._charge;
-         reportSingletron._placeOfIncident = mapReportCase[targetPoint]._placeOfIncident;
-         reportSingletron._speedDetection = mapReportCase[targetPoint]._speedDetection;
-         reportSingletron._amountOfFine = mapReportCase[targetPoint]._amountOfFine;
-         
-         reporterSingletron.rep_name = mapReporter[targetPoint].rep_name;
-         reporterSingletron.rep_unit = mapReporter[targetPoint].rep_unit;
-          reporterSingletron.rep_address = mapReporter[targetPoint].rep_address;
-           reporterSingletron.rep_tel = mapReporter[targetPoint].rep_tel;
-       reporterSingletron.rep_zipcode = mapReporter[targetPoint].rep_zipcode;
-       
-          conveySingletron.conv_personalNo = mapConvey[targetPoint].conv_personalNo;
-          conveySingletron.conv_plateNo = mapConvey[targetPoint].conv_plateNo;
-           conveySingletron.conv_name = mapConvey[targetPoint].conv_name;
-            conveySingletron.conv_tele= mapConvey[targetPoint].conv_tele;
-            conveySingletron.conv_address= mapConvey[targetPoint].conv_address;
-            
-            
-        TrafficTicket memory TrafficTicketIns;
-        TrafficTicketIns.conveyList = conveySingletron;
-        TrafficTicketIns.reporterList = reporterSingletron;
-        TrafficTicketIns.reportList = reportSingletron;
-        
-        trafficList[trafficID] = TrafficTicketIns;
-        trafficTicketNo.push(trafficID);
-
-        
+    function officer() public {
+        addrSAdmin = msg.sender;
     }
     
-       function setReport_case(string charge , string placeOfIncident , string speedDetection , uint amountOfFine) public returns (uint) {
-		
-	   uint report_caseNo = trafficTicketNo.length;
-		
-        report_case memory reportObject;
-        reportObject._charge = charge;
-        reportObject._placeOfIncident = placeOfIncident;
-        reportObject._speedDetection = speedDetection;
-        reportObject._amountOfFine = amountOfFine;
-        
-        mapReportCase[report_caseNo] = reportObject;
-         
-         return (report_caseNo);
-        
-    }
-   
-    
-    function setReporter(string re_name, string re_unit,string re_addr,string re_tel,string re_zipcode) public returns (uint) {
-        
-        uint reporterNo = trafficTicketNo.length;
-        
-        reporter memory reportObject;
-        reportObject.rep_name = re_name;
-        reportObject.rep_unit = re_unit;
-        reportObject.rep_address=re_addr;
-        reportObject.rep_tel=re_tel;
-        reportObject.rep_zipcode=re_zipcode;
-        
-       mapReporter[reporterNo] = reportObject;
-		
-        return (reporterNo);
-        
-      
-    }
- 
-    function setConveyanceOwner(string conv_persNo , string _plate , string conv_na,string conv_tel,string conv_addr) public returns (uint){
-		
-		uint conveyNo = trafficTicketNo.length;
-		
-        conveyanceOwner memory conveyIns;
-         conveyIns.conv_personalNo = conv_persNo;
-         conveyIns.conv_plateNo = _plate;
-        conveyIns.conv_name=conv_na;
-        conveyIns.conv_tele=conv_tel;
-        conveyIns.conv_address=conv_addr;
-        
-        mapConvey[conveyNo] = conveyIns;
-		
-        return (conveyNo);
-   
-    }
-    
-      function editReportCase(string id , string new_charge , string new_placeOfIncident , string new_speedDetection , uint new_amountOfFine) public returns (bool){
-        trafficList[id].reportList._charge = new_charge;
-        trafficList[id].reportList._placeOfIncident = new_placeOfIncident;
-        trafficList[id].reportList._speedDetection = new_speedDetection;
-        trafficList[id].reportList._amountOfFine = new_amountOfFine;
-        
-        return true;
-        
-    }
-    
-          function editReporter(string id , string new_re_name, string new_re_unit,string new_re_addr, string new_re_tel, string new_re_zipcode) public returns (bool){
-        trafficList[id].reporterList.rep_name = new_re_name;
-        trafficList[id].reporterList.rep_unit = new_re_unit;
-        trafficList[id].reporterList.rep_address = new_re_addr;
-        trafficList[id].reporterList.rep_tel = new_re_tel;
-         trafficList[id].reporterList.rep_zipcode = new_re_zipcode;
-         
-         return true;
-        
-    }
-    
-    function editConvey(string id ,string new_conv_personNo , string new_conv_na,string new_conv_tel,string new_conv_addr) public returns (bool) {
-	trafficList[id].conveyList.conv_personalNo = new_conv_personNo;
-         trafficList[id].conveyList.conv_name = new_conv_na;
-        trafficList[id].conveyList.conv_tele = new_conv_tel;
-        trafficList[id].conveyList.conv_address = new_conv_addr;
-        
-        return true;
-
-        
-    }
-	
-	
-    
-    function destroyTrafficTicket() public onlyOwner returns (bool){
-        uint i = 0;
-        TrafficTicket memory ticketNull;
-        for (i ; i<trafficTicketNo.length;i++){
-            trafficList[trafficTicketNo[i]] = ticketNull;
+    function login(string _user , string _pass) public returns (bool , string){
+        string memory pass = userandpass[_user];
+        if(pass.toSlice().equals(_pass.toSlice())){
+            prevLogin[msg.sender] = _user;
+            return (true , mapOfficerToUnit[_user]);
+        }
+        else {
+            return (false , "Not found" ) ;
         }
     }
     
+    modifier checkPermission(string _usr){
+        require(permissionUser[_usr]);
+        _;
+    } 
+    
+    function editOfficer (string unitNo , string _usr 
+    , string _name 
+    , string _unit 
+    , string _office_address 
+    , string _tel
+    , string _zipcode) public onlyOwner checkOffice(_usr) returns (bool) {
+        officerUser[unitNo][_usr].name = _name;
+        officerUser[unitNo][_usr].unit = _unit;
+        officerUser[unitNo][_usr].office_address = _office_address;
+        officerUser[unitNo][_usr].tel = _tel;
+        officerUser[unitNo][_usr].zipcode = _zipcode;
+        return true;
+    }
+        
+    function recoverUsr(string _usr , string _pass,string _repass) public checkOffice(_usr) returns (bool,string){
+        string memory prvUsr = prevLogin[msg.sender];
+        require(prvUsr.toSlice().equals(_usr.toSlice()));
+        require(_pass.toSlice().equals(_repass.toSlice()));
+        userandpass[_usr] = _pass;
+        return (true , _pass);
+    }
+    
+    function newPermission(string _usr) public onlyOwner returns (bool){
+        permissionUser[_usr] = true;
+    }
+    
+    function newOfficer
+    (string _officerCreateID
+    , string unitNo
+    , string _usr 
+    ,string _pass
+    , string _name 
+    , string _unit 
+    , string _office_address 
+    , string _tel
+    , string _zipcode) public checkPermission(_officerCreateID) checkOffice(_officerCreateID) returns (bool) {
+        userandpass[_usr] = _pass;
+        officerUser[unitNo][_usr].username = _usr;
+        officerUser[unitNo][_usr].name = _name;
+        officerUser[unitNo][_usr].unit = _unit;
+        officerUser[unitNo][_usr].office_address = _office_address;
+        officerUser[unitNo][_usr].tel = _tel;
+        officerUser[unitNo][_usr].zipcode = _zipcode;
+        permissionUser[_usr] = false;
+        officerUsernameList.push(_usr);
+        mapOfficerToUnit[_usr] = unitNo;
+        return true;
+        
+    }
+    
+        function getOfficerInfo(string unitNo , string username) public view returns ( string ,string , string ,string){
+        return 
+        (officerUser[unitNo][username].name
+         , officerUser[unitNo][username].office_address 
+         , officerUser[unitNo][username].tel
+         , officerUser[unitNo][username].zipcode);
+    }
+    
+    function getUnitInfo (string unitNo) public view returns (string , string ,string ,string ){
+     return (unitNoToInfo[unitNo].unitName
+         ,unitNoToInfo[unitNo].unitAddr
+         ,unitNoToInfo[unitNo].unitTel
+         ,unitNoToInfo[unitNo].unitZipcode); 
+    }
+    
+    function destroyOfficer(string unitNo) public onlyOwner returns (bool) {
+                uint i = 0;
+        officerInfo memory officerNull;
+        for (i ; i<officerUsernameList.length;i++){
+            officerUser[unitNo][officerUsernameList[i]] = officerNull;
+        }
+    }
 }
