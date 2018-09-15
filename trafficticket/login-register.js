4173e7edc5b1e225e152fe91339d435d2bf400db
dlastwishes <dlasterwishes@gmail.com>
2018-09-15 16:46:22 +0700

    modified dashboard.html and contract

--- trafficticket/login-register.js
+++ trafficticket/login-register.js
@@ -1,62 +1,36 @@
-/*
- *
- * login-register modal
- * Autor: Creative Tim
- * Web-autor: creative.tim
- * Web script: http://creative-tim.com
- * 
- */
-function showRegisterForm(){
-    $('.loginBox').fadeOut('fast',function(){
-        $('.registerBox').fadeIn('fast');
-        $('.login-footer').fadeOut('fast',function(){
-            $('.register-footer').fadeIn('fast');
-        });
-        $('.modal-title').html('Register with');
-    }); 
-    $('.error').removeClass('alert alert-danger').html('');
-       
-}
-function showLoginForm(){
-    $('#loginModal .registerBox').fadeOut('fast',function(){
-        $('.loginBox').fadeIn('fast');
-        $('.register-footer').fadeOut('fast',function(){
-            $('.login-footer').fadeIn('fast');    
-        });
-        
-        $('.modal-title').html('Login with');
-    });       
-     $('.error').removeClass('alert alert-danger').html(''); 
-}
 
-function openLoginModal(){
-    showLoginForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
+
+function login(){
+	 shakeModal();
+    var Web3 = require('web3');
+
+    if (typeof web3 !== 'undefined') {
+
+      web3 = new Web3(web3.currentProvider);
+      console.log("existing web3: provider " + web3);
+      web3.eth.defaultAccount = web3.eth.accounts[0];
+    }
+
+    else {
+
+      web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/g5rWkSjjhx1LPmDch30E"));
+      console.log("new provider " + typeof web3);
+
+    }
+
+    console.log(web3.isConnected());
+
+    if (web3.isConnected()) {
+
+      var officerContract = web3.eth.contract();
+      var officerAddr = "";
+      var officer = officerContract.at(officerAddr);
+
+    } else {
+      console.log('not connect');
+    }
+	
     
-}
-function openRegisterModal(){
-    showRegisterForm();
-    setTimeout(function(){
-        $('#loginModal').modal('show');    
-    }, 230);
-    
-}
-
-function loginAjax(){
-    /*   Remove this comments when moving to server
-    $.post( "/login", function( data ) {
-            if(data == 1){
-                window.location.replace("/home");            
-            } else {
-                 shakeModal(); 
-            }
-        });
-    */
-
-/*   Simulate error message from the server   */
-     shakeModal();
 }
 
 function shakeModal(){
